855daaffdda5d7c77c785df185fbf3b126f13d4d
MAlexGG <alexandragalarzag@gmail.com>
2024-07-08 12:38:53 +0200

    about me instructions updated

--- AboutMe.java
+++ AboutMe.java
@@ -1,6 +1,6 @@
 public class AboutMe {
     public static void main(String[] args) {
-        //inicializar una variable (del tipo que veas conveniente) que contenga tu nombre, edad, país, lenguaje de programación, horas, pasatiempo, lugar y reemplaza en el texto de impresión, la variable que corresponda.
+        //inicializar una variable (del tipo que veas conveniente) para cada uno de los siguentes datos: nombre, edad, país, lenguaje de programación, horas, pasatiempo, lugar y reemplaza en el texto de impresión, la variable que corresponda.
 
         //Escribe tu código aquí
 
